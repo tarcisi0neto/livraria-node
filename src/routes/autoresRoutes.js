ab057c19f55deb3bf5da1d81e92d4c45c1d3e31d
tarcisi0neto <tarcisioxsn@gmail.com>
2023-09-09 17:37:53 -0300

    fix: corrigindo os endpoints do routes de autores

--- src/routes/autoresRoutes.js
+++ src/routes/autoresRoutes.js
@@ -6,9 +6,9 @@ const router = express.Router();
 router 
     .get("/autores", AutoresController.listaAutores)
     .post("/autores", AutoresController.criaAutor)
-    .put("/livros/:id", AutoresController.editaAutor)
-    .get("/livros/:id", AutoresController.buscaAutorId)
-    .delete("/livros/:id", AutoresController.deletarAutor)
+    .put("/autores/:id", AutoresController.editaAutor)
+    .get("/autores/:id", AutoresController.buscaAutorId)
+    .delete("/autores/:id", AutoresController.deletarAutor)
 
 
 export default router;
